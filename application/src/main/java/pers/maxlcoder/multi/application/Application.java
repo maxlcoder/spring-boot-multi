d17e9a67c4782fb56838cfc1da2e61af6676d50b
liurenlin <liurenlin@abukito.com>
2021-11-25 08:22:25 +0800

    init

---
+++ application/src/main/java/pers/maxlcoder/multi/application/Application.java
@@ -0,0 +1,26 @@
+package pers.maxlcoder.multi.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pers.maxlcoder.multi.service.MyService;
+
+@SpringBootApplication(scanBasePackages = "pers.maxlcoder.multi")
+@RestController
+public class Application {
+    private final MyService myService;
+
+    public Application(MyService myService) {
+        this.myService = myService;
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return myService.message();
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ library/src/test/java/pers/maxlcoder/multi/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.maxlcoder.multi.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
